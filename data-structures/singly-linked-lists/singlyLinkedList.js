41244e0b7238ef307e18dc3cae2bc0edcd8fe3b3
Jake Gosnell <jake@hangar31development.com>
2020-05-11 16:55:03 -0700

    started defining singlyLinkedLists

---
+++ data-structures/singly-linked-lists/singlyLinkedList.js
@@ -0,0 +1,25 @@
+/* Singly Linked List */
+
+/* Linked List - an ordered data structure that stores data
+ and contains a head, tail, and length property
+
+Linked Lists consists of nodes, and each node has a value
+ and a pointer to another node or null.
+
+In a singly linked list...
+Each node is only connected to the next node
+In a doubly linked list...
+Each node also can go backwards
+
+Comparison with Arrays...
+Lists
+- do not have indices
+- connected via nodes with a next pointer
+- random access is not allowed
+
+Arrays
+- indexed in order
+- inserts and deletions can be expensive from 
+ the beginning and middle of the array
+- can quickly be accessed at a specific index */ 
+
