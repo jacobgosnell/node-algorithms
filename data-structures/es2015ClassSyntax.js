518856ad4bdd19e5ed0fa33c2899411ec098c67a
Jake Gosnell <jake@hangar31development.com>
2020-05-11 16:42:45 -0700

    added a static method to es2015ClassSyntax.js

--- data-structures/es2015ClassSyntax.js
+++ data-structures/es2015ClassSyntax.js
@@ -4,6 +4,10 @@
   - the constructor method is a special method
   that will be used to instantiate new student
   instances.
+  - inside of all instance methods and constructor, 
+  the keyword `this` refers to the object created 
+  from that class (also known as an instance)
+  - instance = object created from a class
 */
 
 class Student { 
@@ -31,10 +35,34 @@ class Student {
     let sum = this.scores.reduce((a,b) => a+b)
     return sum / this.scores.length;
   }
+  // static Methods
+  // - static methods are called without instantiating their class 
+  // and cannot be called through a class instance.
+  // They are often used to create utility functions for an application.
+  static enrollStudents() {
+    return "ENROLLING STUDENTS!"
+  }
 }
 
 // to create a new instance, use the new keyword
 let firstStudent = new Student("Jake", "Gosnell", 4);
 let secondStudent = new Student("Shane", "Academia", 4);
 
+// Another use case for a utility methods
+class Point {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static distance(a,b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+
+    return Math.hypot(dx, dy);
+  }
+}
+
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
 
---
+++ data-structures/singlyLinkedList.js
@@ -0,0 +1,10 @@
+/* Singly Linked List 
+
+Linked List - an ordered data structure that stores data
+ and contains a head, tail, and length property
+
+Linked Lists consists of nodes, and each node has a value
+ and a pointer to another node or null.
+
+*/ 
+
