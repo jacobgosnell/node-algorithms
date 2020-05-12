44f1ec1830d1284518584b7e1519b684a8861261
Jake Gosnell <jake@hangar31development.com>
2020-05-11 18:10:00 -0700

    created a singly-linked-list with a push method

--- data-structures/singly-linked-lists/singlyLinkedList.js
+++ data-structures/singly-linked-lists/singlyLinkedList.js
@@ -23,3 +23,38 @@ Arrays
  the beginning and middle of the array
 - can quickly be accessed at a specific index */ 
 
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+  push(val) {
+    let newNode = new Node(val)
+    if(!this.head) {
+      this.head = newNode
+      this.tail = this.head
+    } else {
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+}
+
+let list = new SinglyLinkedList
+list.push("Hello")
+list.push("Goodbye")
+// let first = new Node("Hi")
+// first.next = new Node("there")
+// first.next.next = new Node("how")
+// first.next.next.next = new Node("are")
+// first.next.next.next.next = new Node("you")
