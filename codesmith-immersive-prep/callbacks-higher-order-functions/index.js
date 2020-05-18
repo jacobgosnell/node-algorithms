b8a22ed81851497dbc34eaee30fb8a18997c66d8
Jake Gosnell <jake@hangar31development.com>
2020-05-18 14:19:51 -0700

    added codesmith immersive prep notes to repo

---
+++ codesmith-immersive-prep/callbacks-higher-order-functions/index.js
@@ -0,0 +1,49 @@
+// Let's create a function that returns 9 squared
+function ninesquared() {
+  return 9*9;
+}
+ninesquared(); // 81
+
+// Now 8 squared... and so on
+// this breaks the DRY principle
+
+// Create a generalization
+
+function copyArrayAndMultiplyBy2(array) {
+  let output = [];
+  for (let i = 0; i < array.length; i++) {
+    output.push(array[i] * 2);
+  }
+  return output;
+}
+
+const myArray = [1, 2, 3]
+let result = copyArrayAndMultiplyBy2(myArray)
+
+function copyArrayAndDivideBy2(array) {
+  let output = [];
+  for (let i = 0; i < array.length; i++) {
+    output.push(array[i] /2);
+  }
+  return output
+}
+let resultTwo = copyArrayAndDivideBy2(myArray)
+
+// better generalization
+
+function copyArrayAndManipulate(array, instructions) {
+  let output = [];
+  for (let num of array.length) {
+    output.push(instructions(array[num]));
+  }
+  return output;
+}
+
+function multiplyBy2(input) {
+  return input * 2;
+}
+
+let resultThree = copyArrayAndManipulate([1, 2, 3], multiplyBy2);
+
+// obviously using callbacks and higher order functions allows us a tool in order towrite
+// code that is follows the DRY principle.
