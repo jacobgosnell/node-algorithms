37beaad215f079b8acf5072b7931423e9f434653
Jake Gosnell <jake@hangar31development.com>
2020-11-09 22:27:53 -0700

    added a memoization func

---
+++ frontend-masters/memoization.js
@@ -0,0 +1,31 @@
+// Memoization: caching the value that a func returns
+
+// Task 1: Write a function, times10, that takes an argument, n, and multiples n times 10
+// a simple multiplication fn
+const times10 = (n) => n * 10; // times10(9) === 90
+
+console.log('~~~~~~~~~~~~~~TASK 1~~~~~~~~~~~~~~');
+console.log('times10 returns:', times10(9));
+
+// Task 2: Use an object to cache the results of your times10 function. 
+// protip 1: Create a function that checks if the value for n has been calculated before.
+// protip 2: If the value for n has not been calculated, calculate and then save the result in the cache object.
+
+const cache = {};
+const memoTimes10 = (n) => {
+  if(n in cache) {
+    return cache[n]; //  90
+  } else {
+    console.log('Calculating result');
+    let result = times10(n)
+    cache[n] = result; // populate the object
+    return result;
+  }
+}
+
+console.log('~~~~~~~~~~~~~~TASK 2~~~~~~~~~~~~~~');
+console.log('Task 2 calculated value:', memoTimes10(9));	// calculated
+console.log('Task 2 cached value:', memoTimes10(9));	// cached
+console.log(cache);
+
+
