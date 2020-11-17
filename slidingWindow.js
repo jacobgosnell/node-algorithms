c824aea65d4224366dbbc6cdf5619100fe312a1a
Jake Gosnell <jake@hangar31development.com>
2020-11-16 20:18:53 -0700

    studied sliding window pattern to computate in O(n) time space complexity

--- slidingWindow.js
+++ slidingWindow.js
@@ -1,21 +1,5 @@
 /* Sliding Window
 
-This pattern involves creating a window
-which can either be an array or
-number from one position to another
-
-Depending on a certain condition, the window
-either increases or closes (and a new window
-is created) 
-
-Very useful for keeping track of a subset of data
-in an arr/string etc.
-
-write a function to find the longest sequence of
-unique characters...
-
-"hellothere"
-
 write a function which accepts an array of integers
 and a number. The function should calculate the max
 sum of n consecutive elements in the array.
@@ -23,3 +7,29 @@ sum of n consecutive elements in the array.
 maxSubarraySum([1,2,5,2,8,1,5], 2) // 10
 */
 
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  const length = arr.length
+  if (num > length) return null;
+  // calculate max sum of first n elements
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  // set tempSum to the val of maxSum
+  tempSum = maxSum;
+  // loops remainder of array
+  for (let i = num; i < length; i++) {
+    // sliding window algorithm
+    tempSum = tempSum - arr[i - num] + arr[i];
+    // 3 - 1 + 5 = 7
+    // 7 - 2 + 2 = 7
+    // 7 - 5 + 8 = 10
+    // 10 - 2 + 1 = 9
+    // 9 - 8 + 5 = 6
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+console.log(maxSubarraySum([1,2,5,2,8,1,5], 2)) // 10
